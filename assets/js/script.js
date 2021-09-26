1ab97090cdd41c17b5fc0e2d0da216161cb6fb2e
Lauren Groh <legroh@uwm.edu>
2021-09-25 20:26:29 -0500

    convert time block values to times, add background color conditional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 // Display current date
-
 var CurrentDate = moment().format('MMMM Do, YYYY');
 $("#currentDay").append(CurrentDate);
 
+// Set time block format
+var calendarHour = $(".calendar-hour").value;
+var format = 'hh:mm A';
+// moment(calendarHour.format.toString(), "LT");
+var convertedTime = moment(calendarHour, format);
 
+
+// Link time blocks to colors
+var rowColor = $(".row");
+var time = moment();
+
+if(convertedTime.isBefore(time)){
+    rowColor.style.backgroundColor = 'gray';
+}
+if(convertedTime.isSame(time) || convertedTime.isSame(time.add(59, 'minutes'))){
+    rowColor.style.backgroundColor = 'red';
+
+}
+if(convertedTime.isAfter(time.add(59, 'minutes'))){
+    rowColor.style.backgroundColor = 'green';
+}
