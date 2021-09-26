12f4793a1a22b6c83cbb834af0c840d2de0df6e6
Lauren Groh <legroh@uwm.edu>
2021-09-26 08:58:00 -0500

    fix grid, apply classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ var CurrentDate = moment().format('MMMM Do, YYYY');
 $("#currentDay").append(CurrentDate);
 
 // Set time block format
-var calendarHour = $(".calendar-hour").value;
+var calendarHour = $(".calendar-hour").val;
 var format = 'hh:mm A';
 // moment(calendarHour.format.toString(), "LT");
 var convertedTime = moment(calendarHour, format);
 
+console.log(convertedTime.format('MMMM Do YYYY, h:mm:ss a'))
+console.log(time.format('MMMM Do YYYY, h:mm:ss a'))
 
 // Link time blocks to colors
 var rowColor = $(".row");
@@ -23,3 +25,4 @@ if(convertedTime.isSame(time) || convertedTime.isSame(time.add(59, 'minutes'))){
 if(convertedTime.isAfter(time.add(59, 'minutes'))){
     rowColor.style.backgroundColor = 'green';
 }
+
